27fb68bb7699141da4cb9920c81b3037bfa1e675
igafarov90 <igafarov90@mail.ru>
2022-06-18 16:14:59 +0400

    2

--- src/test/java/github/SearchSelenideRepo.java
+++ src/test/java/github/SearchSelenideRepo.java
@@ -1,7 +1,10 @@
 package github;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchSelenideRepo {
@@ -14,11 +17,18 @@ public class SearchSelenideRepo {
         //ввести в поле поиска selenide и нажать Enter
         $("[data-test-selector=\"nav-search-input\"]").setValue("selenide").pressEnter();
         sleep (5000);
-
-
         //Нажимаем линк от первого результата
+        $("href=\"/selenide/selenide\"").click();
+        // сheck: в заголовке есть selenide/selenide
+        SelenideElement $ = $(("[class=\" d-flex flex-wrap flex-items-center wb-break-word f3 text-normal\"]")).shouldHave(text("selenide/selenide"));
 
 
     }
 
+
+
 }
+
+
+
+
